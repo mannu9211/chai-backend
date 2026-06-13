1d0056fae3466607a47c4ebd282cd0d5fb204dcd
mannu9211 <mannu7644806099@gmail.com>
2026-06-13 23:35:55 +0530

    file handling using multer

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import connectDB from "./db/index.js";
 dotenv.config()
 
 connectDB()
-
-
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed", err);
+})
 /*
 import express from "express";
 const app = express()
