3414c5fbad112be5a017af1f128391538cf9a5ae
mannu9211 <mannu7644806099@gmail.com>
2026-06-18 14:47:16 +0530

    implement user authentication with login and logout functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new Schema({
 )
 
 userSchema.pre("save", async function(next) {
-    if(!this.isModified("password")) return next();
+    if(!this.isModified("password")) return;
 
     this.password = await bcrypt.hash(this.password, 10)
 })
@@ -87,7 +87,6 @@ userSchema.methods.generateRefreshToken = function(){
         }
     )
 }
-userSchema.methods.generateRefreshToken = function(){}
 
 
 export const User = mongoose.model("User", userSchema)
